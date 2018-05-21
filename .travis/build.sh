d773f1430e7c5082757602e3bb36a4d267da9945
John Freman <john.freeman.opensource@gmail.com>
2018-05-21 12:52:45 +0000

    Fixed code signing
    Problems with environment variables.

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,13 +6,12 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
+    # See http://www.debonair.io/post/maven-cd/ for instructions
+    openssl aes-256-cbc -K $encrypted_83553d998fe4_key -iv $encrypted_83553d998fe4_iv \
+        -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    gpg --fast-import .travis/codesigning.asc
+
     if [ "$TRAVIS_TAG" != "" ]; then
-
-        # See http://www.debonair.io/post/maven-cd/ for instructions
-        openssl aes-256-cbc -K $encrypted_e84eefe7574d_key -iv $encrypted_e84eefe7574d_iv \
-          -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-        gpg --fast-import .travis/codesigning.asc
-
         ./mvnw deploy -P publish-artifacts --batch-mode --show-version --settings .travis/settings.xml -Denforcer.skip=true
     fi
 fi
