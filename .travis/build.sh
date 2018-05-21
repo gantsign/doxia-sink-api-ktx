52d97ee1bc64f5ac2cbdd8a738d2057526c561c0
John Freman <john.freeman.opensource@gmail.com>
2018-05-21 11:57:57 +0000

    Added GPG signing of artifacts
    For better repository support.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+set -e
+
 ./mvnw install --batch-mode --show-version --settings .travis/settings.xml
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     if [ "$TRAVIS_TAG" != "" ]; then
+
+        # See http://www.debonair.io/post/maven-cd/ for instructions
+        openssl aes-256-cbc -K $encrypted_e84eefe7574d_key -iv $encrypted_e84eefe7574d_iv \
+          -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+        gpg --fast-import .travis/codesigning.asc
+
         ./mvnw deploy -P publish-artifacts --batch-mode --show-version --settings .travis/settings.xml -Denforcer.skip=true
     fi
 fi
